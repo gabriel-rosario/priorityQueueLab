3ba33ca834e70b26b4cab5cd8382c76bb785e836
Gabriel Rosario <gabriel.rosario2@upr.edu>
2018-05-02 22:36:42 -0400

    lab complete

--- src/testerClasses/PriorityQueueTester3.java
+++ src/testerClasses/PriorityQueueTester3.java
@@ -6,15 +6,16 @@ import priorityQueue.SortedListPriorityQueue;
 import priorityQueue.UnsortedListPriorityQueue;
 import priorityQueueInterfaces.PriorityQueue;
 
-public class PriorityQueueTester2 {
+public class PriorityQueueTester3 {
 
 	public static void main(String[] args) {
-		Integer[] arr = {8, 89, 4, 8, 56, 34, 10, 24, 11, 12, 89, 70, 21, 100, 23, 2, 1, 9, 11, 77, 75, 86, 
-				90, 15, 18, 23, 22, 17, 0, 33, 87, 55, 43, 87, 32, 37, 16, 90, 40, 30, 50, 70}; 
+		String[] arr = {"barrio", "pepe", "julia", "maria", "oliva", "meme", "parada", "baile", "enjendro", 
+		         "vagabundo", "nota", "tienda", "zapato", "caballo", "cafe", "diodo", "multiplica"}; 
+ 
 		
-		PriorityQueue<Integer, Integer> pq; 
+		PriorityQueue<String, String> pq; 
 		
-		pq = new UnsortedListPriorityQueue<Integer, Integer>(new IntegerComparator2());    // 1
+		pq = new UnsortedListPriorityQueue<String, String>(new StringComparator1());    // 1
 		//pq = new SortedListPriorityQueue<Integer, Integer>(new IntegerComparator1());    // 2
 		//pq = new HeapListPriorityQueue<Integer, Integer>(new IntegerComparator1());      // 3
 		//pq = new HeapPriorityQueue<Integer, Integer>(new IntegerComparator1());          // 4
